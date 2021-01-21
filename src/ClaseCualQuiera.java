f69ec4ab54f02d2b8c281e9647efcdd6b9cb59bb
Dr4u95 <49389832+Dr4u95@users.noreply.github.com>
2021-01-21 10:47:02 +0100

    Hola :)

--- src/ClaseCualQuiera.java
+++ src/ClaseCualQuiera.java
@@ -4,6 +4,7 @@ public class ClaseCualQuiera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola soy algo");
+		System.out.print("hola soy un cambio :)");
 
 	}
 
