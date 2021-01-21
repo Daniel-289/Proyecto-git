8ae84fd72ffe7b9492cca4d290f47fd6fbd1c011
Dr4u95 <49389832+Dr4u95@users.noreply.github.com>
2021-01-21 11:01:37 +0100

    soy nuevi

---
+++ src/rama2/OtraClase.java
@@ -0,0 +1,12 @@
+package rama2;
+
+public class OtraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Tio te puedes creer que somos codigo de programacion? :O");
+		System.out.print("super yo ;)");
+		
+	}
+
+}
