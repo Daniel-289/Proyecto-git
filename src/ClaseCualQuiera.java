08eeca356f32a67c1b922dfe1fe2bf6dd38d81da
Dr4u95 <49389832+Dr4u95@users.noreply.github.com>
2021-01-21 11:01:55 +0100

    cambio1

--- src/ClaseCualQuiera.java
+++ src/ClaseCualQuiera.java
@@ -4,7 +4,7 @@ public class ClaseCualQuiera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola soy algo");
-		System.out.print("hola soy un cambio :)");
+		System.out.print("hemos hecho un cambio, ahora es normal");
 
 	}
 
